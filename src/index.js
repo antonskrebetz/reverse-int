64ec32cb5a53914631664a1c74a432859504a450
antonskrebetz <antonskrebetz@mail.ru>
2021-02-16 23:30:17 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == '-') {
+            delete arr[i];
+        }
+    }
+    return arr.reverse().join('');
 }
